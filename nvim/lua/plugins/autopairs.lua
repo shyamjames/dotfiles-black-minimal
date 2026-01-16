a015fdcd758b33413cc119d70fe1abcb79bc0425
Shyam James <shyamjames74@gmail.com>
2026-01-16 18:43:02 +0530

    Add smart Enter key mapping to autopairs for VS Code-like indentation

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -2,7 +2,8 @@ return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
   config = function()
-    require("nvim-autopairs").setup({
+    local autopairs = require("nvim-autopairs")
+    autopairs.setup({
       check_ts = true, -- Enable treesitter support
       ts_config = {
         lua = { "string", "source" },
@@ -21,9 +22,28 @@ return {
       },
     })
 
-    -- Integration with nvim-cmp
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    -- Custom CR (Enter) mapping for smart indentation
     local cmp = require("cmp")
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    
+    -- Smart Enter key behavior
+    local Rule = require('nvim-autopairs.rule')
+    local npairs = require('nvim-autopairs')
+    
+    -- Integration with nvim-cmp for confirm_done
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    
+    -- Enhanced CR mapping with proper indentation
+    local remap = vim.api.nvim_set_keymap
+    local npairs_map = npairs.completion_confirm
+    
+    -- Override CR behavior
+    vim.keymap.set("i", "<CR>", function()
+      if cmp.visible() then
+        return cmp.confirm({ select = false })
+      else
+        return npairs.autopairs_cr()
+      end
+    end, { expr = true, noremap = true })
   end,
 }
