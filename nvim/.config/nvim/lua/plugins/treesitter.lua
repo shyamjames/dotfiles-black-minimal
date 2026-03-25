a2f3f8182cb62025b7eea27821603962266e2aad
Shyam James <shyamjames74@gmail.com>
2026-03-25 19:55:13 +0530

    fix: use pcall-guarded config for treesitter (first-launch safe)

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,23 +7,26 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    -- lazy.nvim calls require("nvim-treesitter.configs").setup(opts) for us
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = {
-        "python", "java",
-        "lua", "vim", "vimdoc",
-        "bash", "json", "yaml", "toml",
-        "markdown", "markdown_inline",
-        "html", "css", "javascript",
-        "regex", "comment",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-    },
+    -- pcall guard: skip setup gracefully if not yet installed (first launch)
+    config = function()
+      local ok, ts = pcall(require, "nvim-treesitter.configs")
+      if not ok then return end
+      ts.setup({
+        ensure_installed = {
+          "python", "java",
+          "lua", "vim", "vimdoc",
+          "bash", "json", "yaml", "toml",
+          "markdown", "markdown_inline",
+          "html", "css", "javascript",
+          "regex", "comment",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      })
+    end,
   },
 }
